6942453f4fba7d7bcc0d9e18acc38e73839be9dd
Cutieguwu <owen.brooks77@gmail.com>
2025-04-01 14:29:26 -0400

    Correct minimum version req.

--- gamelog/src/lib.rs
+++ gamelog/src/lib.rs
@@ -10,7 +10,7 @@ mod play;
 mod terrain;
 
 #[allow(unused)]
-pub const MIN_VER: semver::Version = semver::Version::new(0, 3, 0);
+pub const MIN_VER: semver::Version = semver::Version::new(0, 4, 0);
 
 pub use action::*;
 pub use event::Event;
