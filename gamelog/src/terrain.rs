bf5af561f0665edbd8d1358a5f3fcd75c8423406
Cutieguwu <owen.brooks77@gmail.com>
2025-04-02 11:17:22 -0400

    Deprecate TerrainState::Distance

--- gamelog/src/terrain.rs
+++ gamelog/src/terrain.rs
@@ -2,8 +2,6 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize, Clone, Default, PartialEq)]
 pub enum TerrainState {
-    #[deprecated(since = "0.2.0", note = "Replaced in favour of TerrainState::Yards")]
-    Distance(u8),
     Yards(u8),
     GoalLine,
     Inches,
