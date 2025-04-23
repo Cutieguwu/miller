b8db749a503c2a53fef03ae9d7045c449f3e1258
Cutieguwu <owen.brooks77@gmail.com>
2025-04-23 19:18:53 -0400

    Deprecate LogFile version checks.

--- gamelog/src/file.rs
+++ gamelog/src/file.rs
@@ -7,23 +7,6 @@ use strum::IntoEnumIterator;
 pub struct LogFile(pub Vec<super::Game>);
 
 impl LogFile {
-    pub fn min_ver(&self) -> semver::Version {
-        let mut lowest = semver::Version::new(u64::MAX, u64::MAX, u64::MAX);
-
-        self.0.iter().for_each(|x| {
-            if x.version.cmp_precedence(&lowest).is_lt() {
-                lowest = x.version.clone()
-            }
-        });
-
-        lowest
-    }
-
-    /// Returns if the LogFile min version is compatible.
-    pub fn is_compatible(&self) -> bool {
-        self.min_ver().cmp_precedence(&crate::MIN_VER).is_lt()
-    }
-
     /// Returns the most common action for a given team.
     pub fn most_frequent_action(&self, team: Team) -> Action {
         let mut most_common_action = Action::Unknown;
